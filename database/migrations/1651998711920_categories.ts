b45e8abce024b905925c53e9119cd52b6066f8cb
Melaine Gérard <pro@melaine-gerard.fr>
2022-05-08 18:23:03 +0200

    AAC-1 - Finished CRUD

--- database/migrations/1651998711920_categories.ts
+++ database/migrations/1651998711920_categories.ts
@@ -6,7 +6,8 @@ export default class Categories extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
+      table.string('name').notNullable()
+      table.text('image_url').notNullable()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
